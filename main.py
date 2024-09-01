58401aa46bd57ca01ba5448fcc11e93c6ab700bd
Sennaf0x <Sennafo@hotmail.com>
2024-09-01 17:01:23 -0400

    atualizando filtro da planilha

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ uploaded_file = st.file_uploader('Insira aqui o arquivo',type=["xlsx"])
 if uploaded_file:
     df = pd.read_excel(uploaded_file)
     
-    descricao = pd.DataFrame({'Descrição': df['Description']})
+    descricao = pd.DataFrame({'Descrição': df['Description'].str.replace("Run Queue - ","").str.replace("sequence", "Sequence:")})
     
     detalhes = pd.DataFrame({'Detalhes' : df['Details'].fillna(0)})
     
@@ -30,17 +30,21 @@ if uploaded_file:
     hora_df = pd.DataFrame({'Hora' : hora[0]}) 
     dia_df = pd.DataFrame({'Dia': dia[1]})
     
-    mp = filtro_df["Detalhes"].str.split("/",n=1, expand=True)
+    #Filtrando detalhes de cada injeção (Matéria-prima, teste, Fornecedor e POP)
+    mp = filtro_df["Detalhes"].str.split("/", expand=True)
+    mp['MP'] = mp[3]
+    mp[['Matéria-prima','Teste','Fornecedor','POP']] = mp['MP'].str.split("_", expand=True)
+    materia_prima_df = pd.DataFrame({'Matéria-prima': mp['Matéria-prima'],'Teste':mp['Teste'],'Fornecedor':mp['Fornecedor'], 'POP':mp['POP']})
     
-    mp_df = mp[1].str.split("/",n=1, expand=True)
-    mp_df1 = mp_df[1].str.split("/",n=1, expand=True)
-    mp_df2 = mp_df1[1].str.split("/",n=1, expand=True)
-    mp_df3 = mp_df2[0]
-    materia_prima_df = pd.DataFrame({'Matéria-prima': mp_df3}) 
+    #Descriçao
+    descricao = filtro_df["Descrição"].str.split(":", expand=True)
+    descricao1 = descricao[1].str.replace(" - COQ","COQ")  
+    descricao2 = descricao1.str.rsplit("- ",n=2, expand=True)
+    descricao_df = pd.DataFrame({'Ação': descricao[0].str.replace("Sequence",""),'Descrição': descricao2[0],'Injeção':descricao2[2]}) 
     
     dia_hora_df = pd.concat([dia_df,hora_df], axis=1)
-    final_df = pd.concat([dia_hora_df,filtro_df, materia_prima_df], axis=1)
-    final_df = final_df.drop(columns=['Data','Detalhes'])
+    final_df = pd.concat([dia_hora_df, descricao_df, materia_prima_df], axis=1)
+    #final_df = final_df.drop(columns=['Data','Detalhes'])
     final_df
 
     
